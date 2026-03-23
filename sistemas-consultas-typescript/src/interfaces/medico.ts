c4cc67495c6eb5932b4e4e53d05b58cb61baf5f9
Bruno Seidi Ferreira Takaya <165084187+B00nie@users.noreply.github.com>
2026-03-23 09:43:50 -0300

    criação do front-end

---
+++ sistemas-consultas-typescript/src/interfaces/medico.ts
@@ -0,0 +1,8 @@
+import { Especialidade } from "src/types/especialidade";
+export interface Medico {
+  id: number;
+  nome: string;
+  crm: string;
+  especialidade: Especialidade;
+  ativo: boolean;
+}
